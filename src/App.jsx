af553ef29461ff264d96cbbefa5d44a311454204
shofiudinmuh <shofiudinmuh@gmail.com>
2025-02-09 10:48:37 +0700

    update : npm install cslx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,39 @@
+import clsx from 'clsx';
 import './index.css';
-import { IconBrandTwitter } from '@tabler/icons-react';
+import { IconBrandFacebook, IconBrandTwitter } from '@tabler/icons-react';
 export default function App() {
     return (
         <div className={'bg-slate-900 grid place-content-center min-h-screen'}>
             <div className='flex gap-x-2'>
-                <Button text='Register' />
+                <Button onClick={() => console.log('Register')} type='submit'>
+                    <IconBrandFacebook />
+                    Register
+                </Button>
                 <Button
-                    text={
-                        <>
-                            <IconBrandTwitter />
-                        </>
-                    }
-                />
+                    className={'bg-pink-600'}
+                    onClick={() => console.log('Login')}
+                    type='submit'
+                >
+                    <IconBrandTwitter />
+                    Login
+                </Button>
             </div>
         </div>
     );
 }
 
 function Button(props) {
+    const { className = 'bg-blue-600', children, text } = props;
     return (
-        <button className={'bg-blue-600 text-white px-4 py-2 rounded'}>
-            {props.text}
+        <button
+            {...props}
+            className={clsx(
+                className,
+                '[&>svg]:w-5 [&>svg]:h-5 [&>svg]:stroke-1 flex items-center text-white px-4 py-2 rounded',
+            )}
+        >
+            {/* If text is exist, render the text, and if not exist, render the children */}
+            {text || children}
         </button>
     );
 }
