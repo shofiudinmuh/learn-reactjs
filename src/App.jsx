fa6c7888a480137845ab6bfd65935704c3d2221d
shofiudinmuh <shofiudinmuh@gmail.com>
2025-02-15 14:51:48 +0700

    update : learn useState

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,29 @@ import './index.css';
 // import { IconBrandFacebook, IconBrandTwitter } from '@tabler/icons-react';
 import Card from './components/Card';
 import Button from './components/Button';
+import PlaceContentCenter from './components/PlaceContentCenter';
+import { useState } from 'react';
 function App() {
-    return (
-        <div className={'bg-slate-100 antialiased text-slate-800 flex items-center justify-center min-h-screen'}>
-            <div className='max-w-xl w-full'>
-                <Card>
-                    <Card.Title>Hello React</Card.Title>
-                    <Card.Body>
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore voluptate labore voluptatum. Nobis porro
-                        minima delectus id exercitationem eligendi explicabo sint ut officiis soluta consectetur, esse cumque
-                        magni maxime dignissimos.
-                    </Card.Body>
+    const [count, setCount] = useState(0);
 
-                    <Card.Footer>
-                        <Button>Register</Button>
-                    </Card.Footer>
-                </Card>
+    function handleClick() {
+        setCount((prevState) => prevState + 1);
+    }
+    return (
+        <PlaceContentCenter>
+            <div className={'text-5xl'}>{count}</div>
+            <div className='mt-5 flex items-center gap-2'>
+                <Button onClick={handleClick}>Count + 1</Button>
+                <Button
+                    onClick={() => {
+                        handleClick();
+                        handleClick();
+                        handleClick();
+                    }}>
+                    Count + 3
+                </Button>
             </div>
-        </div>
+        </PlaceContentCenter>
     );
 }
 
